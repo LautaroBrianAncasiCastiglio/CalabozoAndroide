c416d11636840e42912510a06537328a117c6332
LautaroBrianAncasiCastiglio <lautarocas05@gmail.com>
2023-10-30 22:57:40 -0300

    new file:   Controlador/BaseDatos.js 	modified:   Vista/Acciones/js.js 	modified:   index.html

---
+++ Vista/Acciones/js.js
@@ -0,0 +1,10 @@
+Producto()
+function Producto(){
+    // asignamos todo el array de laBase de datos a una variable 
+    const producto = pro;
+    const ApartadoComics = document.getElementById("apartadoComics");
+
+    ApartadoComics.innerHTML="";
+    
+
+}
